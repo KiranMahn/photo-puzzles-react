0b6e6d554797bbf86c67957620c26c2b205bb59e
Kiran Mahn CS2021 <kiranmahn@gmail.com>
2024-03-29 12:19:02 +0000

    implement quiz

--- src/PictureTrivia.js
+++ src/PictureTrivia.js
@@ -5,29 +5,39 @@ import { View , Text, Pressable, Image } from 'react-native';
 import GuessMap from './GuessMap';
 import myData from './PhotoDetails.json';
 import JSConfetti from 'js-confetti';
+import Quiz from './Quiz';
 let id = 0;
-
+let imgLocation = "[56.6577495263809, -4.635479507522097]";
 let imageSrc = './scotland.png';
-
+let numImages = 9;
 // choose random number
 id = Math.floor(Math.random() * 9);
 console.log("id: " + id);
+let imgIds = [id]
+
 // get image with that id
+
+imgLocation = myData["images"][id]["location"];
+console.log("imageLocation: " + imgLocation);
 
 imageSrc = myData["images"][id]["src"];
 console.log("imageSrc: " + imageSrc);
 
 export default function PictureTrivia(props)  {
     console.log(myData);
-
     const [blur, setBlur] = useState(0.5);
     // result is true for win or false for lost
     const [result, setResult] = useState("Pending");
 
     // ready is true when user wins or has guess wrong 3 times in a row
     const [ready, setReady] = useState(false);
+    let imgHistory = []
+    // atmp is increased with each click
+    const [attmp, setAttempt] = useState(0);
 
+    const [distance, setDistance] = useState(0);
     const[score, setScore] = useState(0);
+    const[loc, setLoc] = useState(imgLocation);
     const[src, setSrc] = useState(imageSrc);
     const canvas = this;
     const jsConfetti = new JSConfetti({canvas});
@@ -36,19 +46,41 @@ export default function PictureTrivia(props)  {
         setResult("Pending");
         setReady(false);
         let nextId = Math.floor(Math.random() * 9);
+        if(imgIds.length == numImages) {
+            setResult("over");
+            setReady(true);
+
+            jsConfetti.addConfetti({
+                confettiRadius: 6,
+              })
+              jsConfetti.addConfetti({
+                confettiRadius: 6,
+                confettiNumber: 500,
+              })
+        }
         while(nextId == id) {
             nextId = Math.floor(Math.random() * 9);
         }
         id = nextId;
-        
+        imgIds.push(id);
         console.log("newid:" + id);
+        imgLocation = myData["images"][id]["location"];
+        setLoc(imgLocation);
         imageSrc = myData["images"][id]["src"];
         setSrc(imageSrc);
         console.log("imageSrc: " + imageSrc);
         console.log("clicked");
         
         setBlur(1);
+        setAttempt(0);
     }
+    
+    
+
+    // set image url 
+    // set image location 
+
+    // get rnd id
     
     const [showInfoPanel, setShowInfoPanel] = useState(true);
 
@@ -92,7 +124,7 @@ export default function PictureTrivia(props)  {
                                     fontSize: 'x-large'
                                 }}>You Win!</Text>
                             <Text>
-                               Do you want to play again?
+                                It took you {attmp} attemps! Do you want to play again?
                             </Text>
                             <Pressable onPress={() => {resetGame();}} 
                                 style={{
@@ -112,6 +144,45 @@ export default function PictureTrivia(props)  {
                     </View>
                 );
             }
+            if(result == "Try"){
+                return (
+                    <View style={{position: 'absolute', zIndex: 1, alignSelf: 'center', marginTop: '10%'}} onload={blurBackground()}>
+                        <View style={{
+                            width: '50vw',
+                            height: '30vh', 
+                            backgroundColor: 'oldlace', 
+                            justifyContent: 'space-evenly', 
+                            alignItems: 'center',
+                            borderRadius: 25,
+                            border: '3px solid moccasin'
+                            }}>
+                            <Text style={{
+                                    fontFamily: 'Arvo-Bold, serif',
+                                    fontWeight: 'bold',
+                                    fontSize: 'x-large'
+                                }}>Try Again!</Text>
+                            <Text>
+                                Your guess was {distance} miles away!
+                            </Text>
+                            <Pressable onPress={() => {resetGame();}} 
+                                style={{
+                                    width: '20%',
+                                    height: '15%',
+                                    backgroundColor: 'moccasin',
+                                    borderRadius: '15px',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}>
+                                <Text style={{
+                                    fontFamily: 'Arvo-Bold, serif',
+                                    fontWeight: 'bold'
+                                }}>Try Again</Text>
+                            </Pressable>
+                        </View> 
+                    </View>
+                );
+
+            }
             if(result == "Lost"){
                 return (
                     <View style={{position: 'absolute', zIndex: 1, alignSelf: 'center', marginTop: '10%'}} onload={blurBackground()}>
@@ -130,7 +201,7 @@ export default function PictureTrivia(props)  {
                                     fontSize: 'x-large'
                                 }}>You Lost!</Text>
                             <Text>
-                                Do you want to play again?
+                                Your last guess was {distance} miles away! Do you want to play again?
                             </Text>
                             <Pressable onPress={() => {resetGame();}} 
                                 style={{
@@ -146,6 +217,31 @@ export default function PictureTrivia(props)  {
                                     fontWeight: 'bold'
                                 }}>Play Again</Text>
                             </Pressable>
+                        </View> 
+                    </View>
+                );
+            }
+            if(result == "over"){
+                return (
+                    <View style={{position: 'absolute', zIndex: 1, alignSelf: 'center', marginTop: '10%'}} onload={blurBackground()}>
+                        <View style={{
+                            width: '50vw',
+                            height: '30vh', 
+                            backgroundColor: 'oldlace', 
+                            justifyContent: 'space-evenly', 
+                            alignItems: 'center',
+                            borderRadius: 25,
+                            border: '3px solid moccasin'
+                            }}>
+                            <Text style={{
+                                    fontFamily: 'Arvo-Bold, serif',
+                                    fontWeight: 'bold',
+                                    fontSize: 'x-large'
+                                }}>Game Over!</Text>
+                            <Text>
+                                Total Score: {score} / {numImages}
+                            </Text>
+
                         </View> 
                     </View>
                 );
@@ -181,7 +277,7 @@ export default function PictureTrivia(props)  {
                                       letterSpacing: 1,
                                       
                                 }}>
-                            Answer trivia about the pictures on the left.
+                            Guess where the picture was taken by selecting a point on the map. You get 3 attempts. Your guess must be within 500 miles of the photo.
                         </Text>
                         <Pressable onPress={() => {console.log("clicked");setShowInfoPanel(false);setBlur(1);}} 
                             style={{
@@ -221,25 +317,18 @@ export default function PictureTrivia(props)  {
                         <Text style={{textAlign: 'center', fontSize: 'large', fontFamily: 'Arvo-Bold, serif', padding: '0.5em'}}>Login</Text>
                     </Pressable>
                 </View>
-                
-                <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-                    <Text style={{textAlign: 'center', fontSize: 'large', fontFamily: 'Arvo-Bold, serif', padding: '0.5em', marginRight: '10%'}}>Score: {score}</Text>
-                </View>
 
-                <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center'}}>
+                <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'flex-start'}}>
                     <View>
                         <Image source={src} style={{width: '40vw', height: '30vw', margin: '2em'}}/>
                     </View>
 
-                    <View style={{alignSelf: 'end', backgroundColor: 'lightgray', width: '30vw', height: '60vh', margin: '2em'}}>
-                        <Text>{}</Text>
-                        <View>
-
-                        </View>
+                    <View style={{margin: 2, alignSelf: 'end'}}>
+                        <Quiz id={id}/>
                     </View>
                 </View>
 
-                <View style={{position: 'absolute', bottom: 0, display: 'flex', width: '100vw', alignItems: 'flex-end'}}>
+                <View style={{position: 'absolute', bottom: 0, display: 'flex', width: '100vw', alignItems: 'flex-end', height: '5%'}}>
                     <Pressable style={{margin: '2em'}} onPress={() => {setShowInfoPanel(true); setBlur(0.5)}}>
                         <Text style={{fontSize: 'x-large', margin: '1em', fontWeight: 'bold'}}>?</Text>
                     </Pressable>
