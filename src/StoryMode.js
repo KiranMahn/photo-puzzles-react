13b55d8e6fd8c2d024572318bbb18bd77685b298
Kiran Mahn CS2021 <kiranmahn@gmail.com>
2024-04-06 12:05:49 -0600

    centerrr

--- src/StoryMode.js
+++ src/StoryMode.js
@@ -122,15 +122,22 @@ const StoryMode = () => {
             <PopUp/>
             <View style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
                 <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-                    <Link to="/" style={{textDecoration: 'none'}}>
-                        <Text style={{margin: '2em', fontSize: '36px', fontWeight: 'bold'}}>&larr;</Text>
-                    </Link>
+                    <View style={{margin: '1em', width: '10%'}}>
+                        <Link to="/" style={{textDecoration: 'none'}}>
+                            <Text style={{fontSize: '36px', fontWeight: 'bold'}}>&larr;</Text>
+                        </Link>
+                    </View>
+                    
                     
                     <Text style={{textAlign: 'center', fontSize: 'xx-large', fontFamily: 'Arvo-Bold, serif', borderBottomWidth:'1px', padding: '0.5em', borderBlockColor: 'lightgray'}}> Story Mode </Text>
-                    <Pressable style={{margin: '2em'}}>
+                    <Pressable style={{margin: '1em', width: '10%'}}>
                         <Text style={{textAlign: 'center', fontSize: 'large', fontFamily: 'Arvo-Bold, serif', padding: '0.5em'}}>Login</Text>
                     </Pressable>
                 </View>
+                <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={{textAlign: 'center', fontSize: 'small', fontFamily: 'Arvo-Bold, serif', padding: '0.5em'}}> Choose your starting image </Text>
+                </View>
+
                 <View style={{flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', width: '100vw'}}>
                     {chosenImages}
                 </View>
