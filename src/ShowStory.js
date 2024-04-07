648f096c746880311aa3d72636e75caf52b6b5e7
Kiran Mahn CS2021 <kiranmahn@gmail.com>
2024-04-07 16:30:25 -0600

    display correct prompt

--- src/ShowStory.js
+++ src/ShowStory.js
@@ -11,12 +11,20 @@ const generateCaroselItems = (chosenImageIds) => {
   let items = [];
   for(let i = 0; i < len; i++) {
     console.log("current chosenImageIds: ", chosenImageIds[i])
+    let prompt = "";
+    if(i == 0) {
+      prompt = myData["images"][chosenImageIds["chosenImagesIds"][i]]["story"]["start"]
+    } else if(i == len-1) {
+      prompt = myData["images"][chosenImageIds["chosenImagesIds"][i]]["story"]["end"]
+    } else {
+      prompt = myData["images"][chosenImageIds["chosenImagesIds"][i]]["story"]["middle"]
+    }
     items.push(
       <Carousel.Item style={{width: '100vw', height: '100vh', padding: '1em', display: 'flex', justifyContent: 'center'}} key={i}>
           <Image source={myData["images"][chosenImageIds["chosenImagesIds"][i]]["src"]} style={{width: '100vw', height: '90vw', alignSelf: 'center', margin: '1em'}}/>
           <Carousel.Caption style={{backgroundColor: 'rgb(0,0,0,50%)', borderRadius: '3.5em'}}>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            {/* <h3>First slide label</h3> */}
+            <p>{prompt}</p>
           </Carousel.Caption>
       </Carousel.Item>
     );
