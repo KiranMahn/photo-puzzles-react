96d19915b9ea34e30f07e755afbd2be3a1f97054
Kiran Mahn CS2021 <kiranmahn@gmail.com>
2024-04-07 19:28:40 -0600

    titlePage

--- src/MemoryGame.js
+++ src/MemoryGame.js
@@ -1,12 +1,22 @@
 import {Link} from 'react-router-dom';
-
+import { View, Image, Pressable, Text } from 'react-native-web';
 const MemoryGame = () => {
     return (
-        <div className="not found">
-            <h2> Sorry </h2>
-            <p>That page cannot be found </p> 
-            <Link to="/">Back to the homepage...</Link>
-        </div>
+        <View style={{height: '100vh', display: 'flex'}}>
+            <View style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
+                <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100vw'}}>
+                    <View style={{margin: '1em', width: '10%'}}>
+                        <Link to="/" style={{textDecoration: 'none'}}>
+                            <Text style={{fontSize: '36px', fontWeight: 'bold'}}>&larr;</Text>
+                        </Link>
+                    </View>
+                    <Text style={{textAlign: 'center', fontSize: 'xx-large', fontFamily: 'Arvo-Bold, serif', borderBottomWidth:'1px', padding: '0.5em', borderBlockColor: 'lightgray'}}> Memory Game </Text>
+                    <Pressable style={{margin: '1em', width: '10%'}}>
+                        <Text style={{textAlign: 'center', fontSize: 'large', fontFamily: 'Arvo-Bold, serif', padding: '0.5em'}}>Login</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </View>
     )
 }
 
