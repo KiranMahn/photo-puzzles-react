031261960355a23f4a47091c9249ab5876828da3
Kiran Mahn CS2021 <kiranmahn@gmail.com>
2024-04-07 22:44:55 -0600

    add confetti and winning message

--- src/MemoryGame.js
+++ src/MemoryGame.js
@@ -4,10 +4,12 @@ import ReactFlipCard from 'reactjs-flip-card'
 import { shuffle } from './PictureTrivia';
 import myData from './PhotoDetails.json';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import JSConfetti from 'js-confetti';
 
 let firstCard = "";
 let secondCard = "";
-
+let correctInARow = 0;
 
 const populateOrder = () => {
     let imageOrder = []
@@ -23,10 +25,112 @@ const populateOrder = () => {
 let imageOrder = populateOrder();
 console.log("imageOrder: " + imageOrder);
 
+
+
 const MemoryGame = () => {
     const [found, setFound] = useState();
+    const [showInfoPanel, setShowInfoPanel] = useState(true);
+    const [blur, setBlur] = useState(0.5);
+    const canvas = this;
+    const jsConfetti = new JSConfetti({canvas});
     let cards = [];
     
+
+    const PopUp = () => {
+        if(showInfoPanel) {
+            setBlur(0.5);
+            if(correctInARow == 9) {
+                return (
+                    <View style={{position: 'absolute', zIndex: 1, alignSelf: 'center', marginTop: '10%'}}>
+                        <View style={{
+                            width: '50vw',
+                            height: '30vh', 
+                            backgroundColor: 'oldlace', 
+                            justifyContent: 'space-evenly', 
+                            alignItems: 'center',
+                            borderRadius: 25,
+                            border: '3px solid moccasin'
+                            }}>
+                            <Text style={{
+                                    fontFamily: 'Arvo-Bold, serif',
+                                    fontWeight: 'bold',
+                                    fontSize: 'x-large'
+                                }}>How to Play</Text>
+                            <Text style={{fontFamily: 'Arvo-Bold', 
+                                        color: 'gray',
+                                        width: '70%',
+                                        textAlign: 'center',
+                                        letterSpacing: 1,
+                                        
+                                    }}>
+                                You Win!
+                            </Text>
+                            <Pressable onPress={() => {console.log("clicked");setShowInfoPanel(false); setBlur(1)}} 
+                                style={{
+                                    width: '20%',
+                                    height: '15%',
+                                    backgroundColor: 'moccasin',
+                                    borderRadius: '15px',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}>
+                                <Text style={{
+                                    fontFamily: 'Arvo-Bold, serif',
+                                    fontWeight: 'bold'
+                                }}>Close Me</Text>
+                            </Pressable>
+                        </View> 
+                    </View>
+                );
+
+            } else {
+                return (
+                    <View style={{position: 'absolute', zIndex: 1, alignSelf: 'center', marginTop: '10%'}}>
+                        <View style={{
+                            width: '50vw',
+                            height: '30vh', 
+                            backgroundColor: 'oldlace', 
+                            justifyContent: 'space-evenly', 
+                            alignItems: 'center',
+                            borderRadius: 25,
+                            border: '3px solid moccasin'
+                            }}>
+                            <Text style={{
+                                    fontFamily: 'Arvo-Bold, serif',
+                                    fontWeight: 'bold',
+                                    fontSize: 'x-large'
+                                }}>How to Play</Text>
+                            <Text style={{fontFamily: 'Arvo-Bold', 
+                                        color: 'gray',
+                                        width: '70%',
+                                        textAlign: 'center',
+                                        letterSpacing: 1,
+                                        
+                                    }}>
+                                Select matching images. Get all the matches correct in a row to win. 
+                            </Text>
+                            <Pressable onPress={() => {console.log("clicked");setShowInfoPanel(false); setBlur(1)}} 
+                                style={{
+                                    width: '20%',
+                                    height: '15%',
+                                    backgroundColor: 'moccasin',
+                                    borderRadius: '15px',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}>
+                                <Text style={{
+                                    fontFamily: 'Arvo-Bold, serif',
+                                    fontWeight: 'bold'
+                                }}>Close Me</Text>
+                            </Pressable>
+                        </View> 
+                    </View>
+                );
+            }
+        } else {
+            return;
+        }
+    }
 
 
     const setActiveCard = (key) => {
@@ -44,6 +148,7 @@ const MemoryGame = () => {
             console.log("comparing...")
             if(parseInt(firstCard) === parseInt(secondCard)) {
                 console.log("you get a point!!");
+                correctInARow++;
                 firstCard = "";
                 secondCard = "";
                 // temp.push(firstCard);
@@ -55,8 +160,22 @@ const MemoryGame = () => {
                 }, "500");
                 firstCard = "";
                 secondCard = "";
+                correctInARow = 0;
             }
             setFound(undefined);
+            if(correctInARow == 9) {
+                console.log("you win")
+                jsConfetti.addConfetti({
+                    emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+                 })
+                 jsConfetti.addConfetti({
+                    confettiRadius: 6,
+                  })
+                  jsConfetti.addConfetti({
+                    confettiRadius: 6,
+                    confettiNumber: 500,
+                  })
+            }
         }
     
     }
@@ -78,7 +197,8 @@ const MemoryGame = () => {
     }
     return (
         <View style={{height: '100vh', display: 'flex'}}>
-            <View style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
+            <PopUp/>
+            <View style={{height: '100vh', display: 'flex', flexDirection: 'column', opacity: blur}}>
                 <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100vw', marginBottom: '1em'}}>
                     <View style={{margin: '1em', width: '10%'}}>
                         <Link to="/" style={{textDecoration: 'none'}}>
