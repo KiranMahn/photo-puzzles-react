2848b572b181230a5dcb3162da22fbff4586cbf7
Kiran Mahn CS2021 <kiranmahn@gmail.com>
2024-05-11 15:56:34 +0100

    update notfound css

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,9 +2,10 @@ import {Link} from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div className="not found">
+        <div className="not found" style={{backgroundColor: 'pink', borderRadius: '3.5em', padding: '1em'}}>
             <h2> Sorry </h2>
             <p>That page cannot be found </p> 
+            <p>Womp Womp </p> 
             <Link to="/">Back to the homepage...</Link>
         </div>
     )
